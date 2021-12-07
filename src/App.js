e1d69c0e76fdb9c567e334b336fd7c8af6f86d01
Annabel Krijgsman <annabelkrijgsman@gmail.com>
2021-12-07 16:45:56 +0100

    Added base for routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { Routes, Route, Link } from "react-router-dom";
 import SongOverview from './Components/SongOverview'
 
 function App() {
   return (
-      <SongOverview />
+      <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  )
+}
+
+function Home() {
+  return (
+    <>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+      <main>
+        <h1>Songsaver</h1>
+        <SongOverview />
+      </main>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+    </>
   );
 }
 
